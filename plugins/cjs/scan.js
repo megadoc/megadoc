d74f18e3d6599b2773d9f5693c8362c0a8c4ca25
Ahmad Amireh <ahmad@amireh.net>
2015-08-26 23:53:51 +0300

    A helper for globbing & filtering files

--- plugins/cjs/scan.js
+++ plugins/cjs/scan.js
@@ -8,40 +8,26 @@ var where = require('lodash').where;
 var pluck = require('lodash').pluck;
 var uniq = require('lodash').uniq;
 
+
 module.exports = function(config, gitRepository, utils, done) {
+  var database;
   var console = new Logger('cjs scanner');
-  var sourcePatterns = arrayWrap(config.source);
+  var svc = Promise.resolve();
+  var files = utils.globAndFilter(config.source, config.exclude);
 
-  var files = sourcePatterns.reduce(function(sources, pattern) {
-    return sources.concat(glob.sync(utils.assetPath(pattern), { nodir: true }));
-  }, []);
+  console.log('Parsing docs from %d files.', files.length);
 
-  var filters = arrayWrap(config.exclude || []);
-
-  var matchedFiles = files.filter(function(filePath) {
-    return !filters.some(function(filter) {
-      return filePath.match(filter);
-    });
-  });
-
-  console.log('Parsing docs from %d files (%d were filtered).',
-    matchedFiles.length,
-    files.length - matchedFiles.length
-  );
-
-  var database = matchedFiles.reduce(function(database, filePath) {
+  database = files.reduce(function(database, filePath) {
     return database.concat(DoxParser.parseFile(filePath, config));
   }, []);
 
   DoxParser.postProcess(database);
 
-  var svc = Promise.resolve();
-
   if (config.gitStats) {
     console.log("Parsing git stats...");
 
     // cuz some files might have been filtered, we don't want to use
-    // matchedFiles - stat only what we actually parsed
+    // files - stat only what we actually parsed
     var filePaths = uniq(pluck(database, 'filePath'));
 
     svc = parseGitStats(gitRepository, filePaths).then(function(stats) {
--- plugins/markdown/scan.js
+++ plugins/markdown/scan.js
@@ -11,82 +11,59 @@ var parseTitle = require('./scan/parseTitle');
 var parseSections = require('./scan/parseSections');
 var Promise = require('bluebird');
 
-function scanCollection(collectionConfig, utils, markdownConfig, globalConfig, done) {
-  var pattern = utils.assetPath(collectionConfig.source);
-  var filters = collectionConfig.exclude || [];
+function scanCollection(collection, utils, markdownConfig, globalConfig, done) {
+  var files = utils.globAndFilter(collection.source, collection.exclude);
+  var database = files.map(function(filePath) {
+    return {
+      filePath: filePath,
+      source: fs.readFileSync(filePath, 'utf-8')
+    };
+  }, []);
 
-  if (!Array.isArray(filters)) {
-    filters = [ filters ];
+  var commonPrefix = findCommonPrefix(pluck(database, 'filePath'));
+  var id;
+
+  database.forEach(function(entry) {
+    if (commonPrefix && commonPrefix.length) {
+      id = entry.filePath.replace(commonPrefix, '');
+    }
+    else {
+      id = entry.filePath;
+    }
+
+    var fileName = entry.filePath.replace(commonPrefix || '', '').split('/');
+    fileName = fileName[fileName.length-1].replace(/\.[\w]{1,3}$/, '').replace(/\W/g, '-');
+
+    // remove the extension
+    entry.id = id.replace(/\.[\w]{1,3}$/, '').replace(/\W/g, '-');
+    entry.title = parseTitle(entry.source, fileName);
+    entry.sections = parseSections(entry.source);
+    entry.folder = strHumanize(
+      id.indexOf('/') > -1 ?
+        id.split('/')[0] :
+        'root'
+    );
+  });
+
+  console.debug(JSON.stringify(database.map(function(e) { return e.id; })));
+
+  var svc = Promise.resolve();
+
+  if (markdownConfig.gitStats) {
+    console.log("Parsing git stats...");
+
+    var filePaths = uniq(pluck(database, 'filePath'));
+
+    svc = parseGitStats(globalConfig.gitRepository, filePaths).then(function(stats) {
+      stats.forEach(function(fileStats) {
+        where(database, { filePath: fileStats.filePath }).forEach(function(entry) {
+          entry.git = fileStats;
+        });
+      });
+    });
   }
 
-  glob(pattern, { nodir: true }, function (err, files) {
-    var matchedFiles, database;
-
-    if (err) {
-      console.error(err);
-      return done(err, null);
-    }
-
-    if (filters.length) {
-      matchedFiles = files.filter(function(fileName) {
-        return !filters.some(function(filter) {
-          return fileName.match(filter);
-        });
-      });
-    }
-
-    database = matchedFiles.map(function(filePath) {
-      return {
-        filePath: filePath,
-        source: fs.readFileSync(filePath, 'utf-8')
-      };
-    }, []);
-
-    var commonPrefix = findCommonPrefix(pluck(database, 'filePath'));
-    var id;
-
-    database.forEach(function(entry) {
-      if (commonPrefix && commonPrefix.length) {
-        id = entry.filePath.replace(commonPrefix, '');
-      }
-      else {
-        id = entry.filePath;
-      }
-
-      var fileName = entry.filePath.replace(commonPrefix || '', '').split('/');
-      fileName = fileName[fileName.length-1].replace(/\.[\w]{1,3}$/, '').replace(/\W/g, '-');
-
-      // remove the extension
-      entry.id = id.replace(/\.[\w]{1,3}$/, '').replace(/\W/g, '-');
-      entry.title = parseTitle(entry.source, fileName);
-      entry.sections = parseSections(entry.source);
-      entry.folder = strHumanize(
-        id.indexOf('/') > -1 ?
-          id.split('/')[0] :
-          'root'
-      );
-    });
-
-    console.debug(JSON.stringify(database.map(function(e) { return e.id; })));
-
-    var svc = Promise.resolve();
-
-    if (markdownConfig.gitStats) {
-      console.log("Parsing git stats...");
-
-      var filePaths = uniq(pluck(database, 'filePath'));
-
-      svc = parseGitStats(globalConfig.gitRepository, filePaths).then(function(stats) {
-        stats.forEach(function(fileStats) {
-          where(database, { filePath: fileStats.filePath }).forEach(function(entry) {
-            entry.git = fileStats;
-          });
-        });
-      });
-    }
-
-    svc.then(function() { done(null, database); }, done);
-  });
+  svc.then(function() { done(null, database); }, done);
 }
 
 module.exports = function(config, utils, globalConfig, done) {
