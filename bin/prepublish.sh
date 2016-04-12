ffe2bd4200b3747ded257a056bbdad691e99913c
Ahmad Amireh <ahmad@amireh.net>
2016-04-12 04:19:21 +0300

    fixing travis to install package deps

--- bin/prepublish.sh
+++ bin/prepublish.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 if [ -z $PACKAGE ]; then
-  PACKAGE=$1
-
-  if [ -z $PACKAGE ]; then
-    echo "Usage: $0 PACKAGE"
-    exit 1
-  fi
+  echo "Usage: $0 PACKAGE"
+  exit 1
 fi
 
 [ -f "./package.json" ] && grep '"name": "tinydoc"' ./package.json &> /dev/null
@@ -78,25 +74,34 @@ function build_assets {
 echo "Preparing \"${PACKAGE_NAME}\" for publishing... hang on tight."
 echo "---------------------------------------------------------------"
 
+IGNORED=()
+
+while getopts ":S:" opt; do
+  case $opt in
+    S)
+      IGNORED+=("${OPTARG}")
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+function shouldRun() {
+  if [[ " ${IGNORED[@]} " =~ " $1 " ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+echo "Skipping ${IGNORED}"
+
 run_task refresh_dependencies
-run_task lint_sources
-run_task lint_ui_sources
-run_task run_tests
-run_task build_assets
+
+shouldRun "lint" && run_task lint_sources && run_task lint_ui_sources
+shouldRun "test" && run_task run_tests
+shouldRun "build" && run_task build_assets
 
 echo "Package \"${PACKAGE_NAME}\" is good for publishing, nice work!"
 echo "---------------------------------------------------------------"
-
-# echo "Cleaning previous build artifacts..." &&
-# ( [ -d "${PACKAGE_ROOT}/dist" ] && rm -r $PACKAGE_ROOT/dist || echo -n ) &&
-# echo "Linting sources..." &&
-# ./node_modules/.bin/eslint $PACKAGE_ROOT/lib &&
-# echo "Linting UI sources..." &&
-# ./node_modules/.bin/eslint -c .eslintrc -c .eslintrc--ui $PACKAGE_ROOT/ui &&
-# echo "Running tests..." &&
-# ./node_modules/.bin/mocha --reporter dot "${PACKAGE_ROOT}/lib/**/*.test.js" &&
-# echo "Building assets..." &&
-# ./cli/tinydoc-compile --optimize \
-#   "${PACKAGE_ROOT}/dist/${PACKAGE_NAME}.js" \
-#   "${PACKAGE_ROOT}/ui/index.js" &&
-# echo "Done!"
