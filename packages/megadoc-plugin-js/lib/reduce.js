157592abe0b0b9996568606cc781216e397d83ef
Ahmad Amireh <ahmad@amireh.net>
2016-05-24 22:55:50 +0300

    JS: do not overwrite modules that are also namespaces
    An explanation is left in the source commentary. This was happening a
    lot in TinyMCE's sources.

--- packages/megadoc-plugin-js/lib/reduce.js
+++ packages/megadoc-plugin-js/lib/reduce.js
@@ -6,12 +6,6 @@ var RendererUtils = require('megadoc/lib/RendererUtils');
 
 module.exports = function reduceDocuments(options) {
   var rawDocuments = options.documents;
-  var documents = rawDocuments
-    .filter(function(doc) { return doc.isModule; })
-    .map(reduceModuleDocument)
-  ;
-
-  var namespaces = {};
   var bank = b.namespace({
     id: options.namespaceId,
     name: 'megadoc-plugin-js',
@@ -21,20 +15,28 @@ module.exports = function reduceDocuments(options) {
     indexFields: [ '$uid', '$filePath', 'aliases' ],
     meta: {
       href: options.baseURL,
-      outlets: [
-        {
-          name: 'CJS::Module',
-          options: {},
-        },
-        {
-          name: 'CJS::ClassBrowser',
-          options: {},
-        }
-      ],
-
       defaultLayouts: require('./defaultLayouts')
     }
   });
+
+  var documents = rawDocuments
+    .filter(function(doc) { return doc.isModule; })
+    .map(reduceModuleDocument)
+  ;
+
+  // So, the initial set of "namespaces" are all the module docs that contain
+  // other module docs (implicit namespaces). The rest of the namespaces (ones
+  // that are identified by the @namespace tag) we'll create afterwards.
+  //
+  // Note that we must do these two in separate passes because we do not want
+  // to "overwrite" those docs! See below to know what I mean.
+  var namespaces = documents.reduce(function(map, x) {
+    if (x.properties && x.properties.isModule && !x.properties.namespace) {
+      map[x.properties.id] = x;
+    }
+
+    return map;
+  }, {});
 
   documents.forEach(function(x) {
     var namespaceId = x.properties.namespace;
@@ -48,6 +50,8 @@ module.exports = function reduceDocuments(options) {
           id: namespaceId,
           title: namespaceId,
           symbol: K.NAMESPACE_SEP,
+          filePath: x.filePath, // useful for error reporting when there's a UID clash
+          loc: x.loc,
           documents: [],
           entities: [],
           meta: {}
@@ -78,19 +82,19 @@ module.exports = function reduceDocuments(options) {
     });
   }
 
-  function reduceEntityDocument(parentDoc, doc) {
-    return b.documentEntity({
-      id: doc.symbol + doc.name,
-      title: doc.id,
-      summary: generateSummary(doc),
-      filePath: doc.filePath,
-      loc: doc.loc,
-      properties: doc,
-    });
-  }
-
   return bank;
 };
+
+function reduceEntityDocument(parentDoc, doc) {
+  return b.documentEntity({
+    id: doc.symbol + doc.name,
+    title: doc.id,
+    summary: generateSummary(doc),
+    filePath: doc.filePath,
+    loc: doc.loc,
+    properties: doc,
+  });
+}
 
 function generateSummary(doc) {
   if (doc.description) {
