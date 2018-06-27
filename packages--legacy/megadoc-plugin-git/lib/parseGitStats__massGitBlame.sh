3b96e188464edbcb3702d22bc5e4e454c7735bad
Ahmad Amireh <ahmad@amireh.net>
2018-06-27 15:18:09 +0300

    remove legacy git plugin dead code

--- packages--legacy/megadoc-plugin-git/lib/parseGitStats__massGitBlame.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for file in $1; do
-  echo "file: ${file}"
-  git blame -p -c $file | egrep 'committer( |-mail|-time)'
-done
--- packages--legacy/megadoc-plugin-git/ui/index.js
+++
@@ -1,5 +0,0 @@
-megadoc.outlets.define('Git::Stats');
-
-megadoc.use(function GitPlugin() {
-  require('./outlets/StatsOutlet');
-});
